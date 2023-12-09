f21f88ee3697fdbcc7fc0ffd64bdf0dd7ad1c970
Soban142 <inferiorfoe142@gmail.com>
2023-12-10 00:42:29 +0500

    first commit

---
+++ components/Map.js
@@ -0,0 +1,84 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import MapView, { Marker } from "react-native-maps";
+import tw from "tailwind-react-native-classnames";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectDestination,
+  selectOrgin,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
+import { MapViewDirections } from "react-native-maps-directions";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
+const Map = () => {
+  const dispatch = useDispatch();
+  const origin = useSelector(selectOrgin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50,
+      },
+    });
+  }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    const getTravelTime = async () => {
+      fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${destination.description}origins=${origin.description}units=imperial&key=${GOOGLE_MAPS_APIKEY}`
+      )
+        .then((res) => res.json)
+        .then((data) =>
+          // console.log(data)
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]))
+        );
+    };
+  }, [origin, destination, GOOGLE_MAPS_APIKEY]);
+
+  return (
+    <MapView
+      style={tw`flex-1`}
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="hotpink"
+        />
+      )}
+      {origin?.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+    </MapView>
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({});
